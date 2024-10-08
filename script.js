21e160575152710b7b619104f8e8c88be407fe63
Ilyshka3346 <150544619+Ilyshka3346@users.noreply.github.com>
2024-10-08 15:58:54 +0300

    update

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('showMenuButton').addEventListener('click', function() {
+    // Показать изображение проекта при нажатии на кнопку
+    const projectImage = document.getElementById('projectImage');
+    projectImage.classList.toggle('hidden');
+});
